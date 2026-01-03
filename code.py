538f9e33ae57a79ca903ad3d86d16549e103d9d1
Shalini Sharma <shalinisharma@SL-000848.local>
2026-01-03 06:33:41 +0530

    Chatbook phase2

--- code.py
+++ code.py
@@ -16,16 +16,61 @@ class chatbook:
                          5. Press any key to exit""")
 
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
-            pass
+            self.post()
         elif user_input=="4":
-            pass
+            self.message()
         else:
             exit()
             
+    
+    def signup(self):
+        self.username=input("Enter your email id here--> ")
+        self.password=input("Enter your password here--> ")
+        print("You have successfully signed up")
+        self.menu()
+
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Please signup first by pressing 1 to the main menu")
+        else:
+            uname=input("Enter your email id here--> ")
+            pwd=input("Enter your password here--> ")
+            if self.username==uname and self.password==pwd:
+                print("You have signed in successfully!!")
+                self.loggedin=True
+            else:
+                print("please input correct credentials")
+        
+        self.menu()
+    
+    def post(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here-->")
+            print("Following content has been posted--> {txt}")
+        else:
+            print("In order to post something please login first")
+
+        self.menu()
+
+    def message(self):
+        if self.loggedin==True:
+            txt=input("Enter your message here-->")
+            frnd=input("Whom to send the msg?->")
+            print("Your message has been sent to {frnd}") 
+        else:
+            print("In order to message something please login first")
+
+        self.menu()
+
+
+
+    
+    
+
 
 
 obj=chatbook()
